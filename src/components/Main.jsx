90200f97f62ff8d4028ac88468f7adf7572ec2b8
AugustoStek <augustostekelberg@gmail.com>
2025-06-13 22:17:40 -0300

    Creacion de componente boton y eliminacion de css innecesario

---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import CustomButton from './CustomButton'
+
+const Main = () => {
+    return (
+        <>
+            <div>Main</div>
+            <CustomButton>Este es un boton</CustomButton>
+
+        </>
+    )
+}
+
+export default Main
